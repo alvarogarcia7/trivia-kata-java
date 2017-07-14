b8040d398d13358291fae66b55d59cb03fd19f5d
Alvaro Garcia <derecerca@gmail.com>
2017-07-15 01:21:09 +0200

    Refactor: extract class under test to setup method

--- src/test/java/com/adaptionsoft/games/uglytrivia/PlayersShould.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/PlayersShould.java
@@ -1,5 +1,6 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.stream.IntStream;
@@ -9,14 +10,20 @@ import static org.junit.Assert.assertThat;
 
 public class PlayersShould {
 
+    private Players players;
+
+    @Before
+    public void setUp() throws Exception {
+        players = new Players();
+    }
+
     @Test
     public void be_empty_by_default() {
-        assertThat(new Players().playersSize(), is(0));
+        assertThat(players.playersSize(), is(0));
     }
 
     @Test
     public void add_a_player() {
-        Players players = new Players();
 
         players.addPlayer("player1");
 
@@ -25,7 +32,6 @@ public class PlayersShould {
 
     @Test
     public void add_many_players() {
-        Players players = new Players();
 
         IntStream.rangeClosed(1,1000).mapToObj(Integer::toString).forEach(players::addPlayer);
 
